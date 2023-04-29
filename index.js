84227f419a3f6e3f5d0e013c38d8eebca0b25453
Zemorath <trentward10@gmail.com>
2023-04-28 19:08:16 -0500

    Control Lab Finished

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(lengthOfRide){
   // Write your code here!
+  if (lengthOfRide <= 400) {
+    return 'This one is on me!'
+  } else if (lengthOfRide > 400 && lengthOfRide < 2000) {
+    return 'That will be twenty bucks.'
+  } else if (lengthOfRide > 2000 && lengthOfRide <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (lengthOfRide > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  return cityName === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipReaction){
   // Write your code here!
+  switch (tipReaction) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
